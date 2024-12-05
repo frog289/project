827a673c96445d5bf1d172edf3ddbd98e21bc30b
kumirtop <kirill.b@autorambler.ru>
2024-12-05 10:51:04 +0300

    ячс

--- prac15/вход.js
+++ prac15/вход.js
@@ -1,39 +1,19 @@
 const form = document.getElementById('loginForm');
-const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
-const rememberMeCheckbox = document.getElementById('rememberMe');
 const passwordError = document.getElementById('passwordError');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Предотвращение отправки формы
+event.preventDefault();
 
-  // Валидация пароля (простая проверка длины)
+  
   const password = passwordInput.value;
   if (password.length < 8) {
     passwordError.textContent = 'Пароль должен быть не менее 8 символов';
+
     return;
   }
-  passwordError.textContent = ''; // Очистка сообщения об ошибке
+  passwordError.textContent = ''; 
 
-  // Сохранение данных, если флажок "Запомнить меня" установлен
-  if (rememberMeCheckbox.checked) {
-    localStorage.setItem('username', usernameInput.value);
-    localStorage.setItem('password', passwordInput.value);
-  } else {
-    localStorage.removeItem('username');
-    localStorage.removeItem('password');
-  }
 
-  // Здесь можно добавить логику обработки успешного входа
   alert('Вы вошли!');
 });
-
-
-// Автоматическое заполнение формы при загрузке страницы
-const savedUsername = localStorage.getItem('username');
-const savedPassword = localStorage.getItem('password');
-if (savedUsername && savedPassword) {
-  usernameInput.value = savedUsername;
-  passwordInput.value = savedPassword;
-  rememberMeCheckbox.checked = true;
-}
